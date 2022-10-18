4afabf9d8e06b06d7de9d11536ce5d1f1ff89e80
Simba-T <116117273+Simba-T@users.noreply.github.com>
2022-10-18 23:01:48 +0000

    Update ESP32_Ultrasonic_LCD.ino

--- ESP32_Ultrasonic_LCD/ESP32_Ultrasonic_LCD.ino
+++ ESP32_Ultrasonic_LCD/ESP32_Ultrasonic_LCD.ino
@@ -1,16 +1,15 @@
 /*
- * This ESP32 code is created by esp32io.com
- 
+ Program for water level monitoring with LCD and Ultrasonic sensor
  */
 
 #include <LiquidCrystal_I2C.h>
 
-LiquidCrystal_I2C lcd(0x27, 16, 2); // I2C address 0x3F, 16 column and 2 rows
+LiquidCrystal_I2C lcd(0x27, 16, 2); // I2C address 0x27, 16 column and 2 rows
 
 #define TRIG_PIN 5 // ESP32 pin GIOP5 connected to Ultrasonic Sensor's TRIG pin
 #define ECHO_PIN 18 // ESP32 pin GIOP18 connected to Ultrasonic Sensor's ECHO pin
 
-float duration_us, distance;
+float duration, distance;
 
 void setup() {
   lcd.init();               // initialize the lcd
@@ -20,21 +19,21 @@ void setup() {
 }
 
 void loop() {
-  // generate 10-microsecond pulse to TRIG pin
+  // generate 10-us pulse to TRIG pin
   digitalWrite(TRIG_PIN, HIGH);
   delayMicroseconds(10);
   digitalWrite(TRIG_PIN, LOW);
 
   // measure duration of pulse from ECHO pin
-  duration_us = pulseIn(ECHO_PIN, HIGH);
+  duration = pulseIn(ECHO_PIN, HIGH);
 
   // calculate the distance
-  distance = 0.017 * duration_us;
+  distance = 0.03463/2 * duration; //speed of sound at 30 celcious around is 346.3m/s 
 
   lcd.clear();
   lcd.setCursor(0, 0); // start to print at the first row
   lcd.print("Tank 1 Monitor ");
-  lcd.setCursor(0, 1); // start to print at the first row
+  lcd.setCursor(0, 1); // start to print at character 0, row 2
   lcd.print("Level: ");
   lcd.print(distance);
   //lcd.print("cm");
