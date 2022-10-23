e46af425fd6a35c18842a005ccc3de6ceef3d9d9
Simba-T <116117273+Simba-T@users.noreply.github.com>
2022-10-23 13:29:46 +0000

    Update ESP32_Ultrasonic_LCD.ino

--- ESP32_Ultrasonic_LCD/ESP32_Ultrasonic_LCD.ino
+++ ESP32_Ultrasonic_LCD/ESP32_Ultrasonic_LCD.ino
@@ -37,15 +37,102 @@ LiquidCrystal_I2C lcd(0x27, 16, 2);  // I2C address 0x27, 16 column and 2 rows
 WebServer server(80);
 
 
+void systm(){
+  //RUN SERVER
+  server.handleClient();
+  delay(2);  //allow the cpu to switch to other tasks
+
+  //CONFIGURE ULTRASONIC
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);  // generate 10-us pulse to TRIG pin
+  digitalWrite(TRIG_PIN, LOW);
+  duration = pulseIn(ECHO_PIN, HIGH);  // measure duration of pulse from ECHO pin
+  // calculate the distance
+  distance = 0.03463 / 2 * duration;  //speed of sound at 30 celcious around is 346.3m/s
+
+  //CONFIGURE SYSTEM DISPLAY
+  lcd.clear();
+  lcd.setCursor(0, 0);  // start to print at the first row
+  lcd.print("Tank 1 Monitor ");
+  lcd.setCursor(0, 1);  // start to print at character 0, row 2
+  lcd.print("Level: ");
+  lcd.print(distance);
+  //lcd.print("cm");
+
+ 
+
+  //CONFIGURE DATABASE STORAGE
+  HTTPClient http;
+  String HOST_NAME1 = "http://192.168.137.34";  // change to your PC's IP address
+  String PATH_NAME1 = "/Lab5/lab5/insert_waterlevel.php";
+  String queryString1 = "?TankID=" + String(TANKID) + "&LocationID=" + String(LOCATIONID) + "&OwnerID=" + String(OWNERID) + "&WaterLevel=" + String(distance);
+
+  http.begin(HOST_NAME1 + PATH_NAME1 + queryString1);  //HTTP
+  int httpCode = http.GET();
+
+  // httpCode will be negative on error
+  if (httpCode > 0) {
+    // file found at server
+    if (httpCode == HTTP_CODE_OK) {
+      String payload = http.getString();
+      Serial.println(payload);
+    } else {
+      // HTTP header has been send and Server response header has been handled
+      Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+    }
+  } else {
+    Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+  }
+
+  http.end();
+  delay(1000);  
+}
+
+
 void handleRoot() {
-  digitalWrite(led, 1);
-  server.send(200, "text/plain", "LED IS ON");
+  while(1){
+systm();
+ //CONFIGURE DEFAULT AUTOMATIC SYSTEM
+  if (distance <= 15) {            //minimum water level 15cm
+    digitalWrite(RED_LED, HIGH);   // turn RED RED_RED_LED ON
+    digitalWrite(GREEN_LED, LOW);  //normal operation
+    digitalWrite(BUZZER, LOW);     // turn OFF BUZZER as active low
+    digitalWrite(MOTOR, HIGH);     // turn OFF MOTOR
+    //digitalWrite(led, HIGH);    // turn OFF MOTOR
+    digitalWrite(led, 1);
+  }
+
+  if (distance >= 100) {
+    digitalWrite(RED_LED, LOW);  // turn RED_LED OFF
+    digitalWrite(GREEN_LED, HIGH);
+    digitalWrite(BUZZER, HIGH);  // turn BUZZER OFF
+    digitalWrite(MOTOR, LOW);    // turn MOTOR OFF
+    digitalWrite(led, 0);    // turn OFF MOTOR
+  }
+  server.send(200, "text/plain", "SYSTEM ON AUTO");
+}
 }
 
 void handleRoot2() {
-  digitalWrite(led, 0);
-  server.send(200, "text/plain", "LED IS OFF");
+    while(1){
+    systm();
+    digitalWrite(MOTOR, HIGH);     // turn OFF MOTOR
+    digitalWrite(led, HIGH);    // turn OFF MOTOR
+  server.send(200, "text/plain", "MOTOR IS ON");
 }
+}
+void handleRoot3() {
+  while(1){
+    systm();
+    digitalWrite(MOTOR, HIGH);     // turn OFF MOTOR
+    digitalWrite(led, 0);    // turn OFF MOTOR
+    server.send(200, "text/plain", "MOTOR IS ON");
+      //delay(500);
+    //digitalWrite(RED_LED, 0);
+  
+}
+}
+
 
 
 
@@ -82,8 +169,9 @@ void setup() {
     Serial.println("MDNS responder started");
   }
 
-  server.on("/ON", handleRoot);
-  server.on("/OFF", handleRoot2);
+  server.on("/AUTO", handleRoot);
+  server.on("/ON", handleRoot2);
+  server.on("/OFF", handleRoot3);
    server.begin();
   Serial.println("HTTP server started");
 
@@ -111,28 +199,8 @@ void setup() {
 }
 
 void loop() {
-  //RUN SERVER
-  server.handleClient();
-  delay(2);  //allow the cpu to switch to other tasks
-
-  //CONFIGURE ULTRASONIC
-  digitalWrite(TRIG_PIN, HIGH);
-  delayMicroseconds(10);  // generate 10-us pulse to TRIG pin
-  digitalWrite(TRIG_PIN, LOW);
-  duration = pulseIn(ECHO_PIN, HIGH);  // measure duration of pulse from ECHO pin
-  // calculate the distance
-  distance = 0.03463 / 2 * duration;  //speed of sound at 30 celcious around is 346.3m/s
-
-  //CONFIGURE SYSTEM DISPLAY
-  lcd.clear();
-  lcd.setCursor(0, 0);  // start to print at the first row
-  lcd.print("Tank 1 Monitor ");
-  lcd.setCursor(0, 1);  // start to print at character 0, row 2
-  lcd.print("Level: ");
-  lcd.print(distance);
-  //lcd.print("cm");
-
-  //CONFIGURE DEFAULT AUTOMATIC SYSTEM
+  systm();
+   //CONFIGURE DEFAULT AUTOMATIC SYSTEM
   if (distance <= 15) {            //minimum water level 15cm
     digitalWrite(RED_LED, HIGH);   // turn RED RED_RED_LED ON
     digitalWrite(GREEN_LED, LOW);  //normal operation
@@ -146,32 +214,7 @@ void loop() {
     digitalWrite(GREEN_LED, HIGH);
     digitalWrite(BUZZER, HIGH);  // turn BUZZER OFF
     digitalWrite(MOTOR, LOW);    // turn MOTOR OFF
-                                 //digitalWrite(led, LOW);    // turn OFF MOTOR
+    //digitalWrite(led, LOW);    // turn OFF MOTOR
   }
 
-  //CONFIGURE DATABASE STORAGE
-  HTTPClient http;
-  String HOST_NAME1 = "http://192.168.137.34";  // change to your PC's IP address
-  String PATH_NAME1 = "/Lab5/lab5/insert_waterlevel.php";
-  String queryString1 = "?TankID=" + String(TANKID) + "&LocationID=" + String(LOCATIONID) + "&OwnerID=" + String(OWNERID) + "&WaterLevel=" + String(distance);
-
-  http.begin(HOST_NAME1 + PATH_NAME1 + queryString1);  //HTTP
-  int httpCode = http.GET();
-
-  // httpCode will be negative on error
-  if (httpCode > 0) {
-    // file found at server
-    if (httpCode == HTTP_CODE_OK) {
-      String payload = http.getString();
-      Serial.println(payload);
-    } else {
-      // HTTP header has been send and Server response header has been handled
-      Serial.printf("[HTTP] GET... code: %d\n", httpCode);
-    }
-  } else {
-    Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
-  }
-
-  http.end();
-  delay(1000);
 }
